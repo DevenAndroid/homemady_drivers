f4b0b88a2d8925ba785afa9a04f48a122868a47c
ppp <deveneoxys@gmail.com>
2023-11-20 15:03:55 +0530

    20 Nov

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:homemady_drivers/routers/routers.dart';
 import 'package:homemady_drivers/services/notification_service.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
@@ -38,15 +39,17 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return GetMaterialApp(
-      title: 'HomeMady Driver',
-      theme: ThemeData(
-        fontFamily: 'poppinsSans',
-        primarySwatch: Colors.green,
+    return OverlaySupport.global(
+      child: GetMaterialApp(
+        title: 'HomeMady Driver',
+        theme: ThemeData(
+          fontFamily: 'poppinsSans',
+          primarySwatch: Colors.green,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: "/",
+        getPages: MyRouters.route,
       ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: "/",
-      getPages: MyRouters.route,
     );
   }
 }
--- lib/screen/deshborad_screen.dart
+++ lib/screen/deshborad_screen.dart
@@ -12,6 +12,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:homemady_drivers/routers/routers.dart';
 import 'package:homemady_drivers/widgets/custome_size.dart';
 import 'package:homemady_drivers/widgets/new_helper.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../controller/assigned_Orderlist_controller.dart';
 import '../controller/deshborad_controoler.dart';
@@ -121,6 +122,105 @@ class _DashbordScreenState extends State<DashbordScreen> {
     log("Notification received..........   onMessage        ${event.toMap()}");
     NotificationService()
         .showNotificationWithRemoteMessage(remoteMessage: event);
+    showNotificationDialog(event);
+  }
+
+
+  showNotificationDialog(RemoteMessage remoteMessage){
+    FirebaseMessaging.instance.getToken().then((value) {
+      log("FCM Token...      ${value}");
+    });
+    showSimpleNotification(
+        GestureDetector(
+          onTap: (){},
+          behavior: HitTestBehavior.translucent,
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: const Color(0xFF04666E).withOpacity(0.14),
+                    spreadRadius: 1.0,
+                    blurRadius: 20.0,
+                    offset: const Offset(3.0, 3.0))
+              ],
+            ),
+            margin: const EdgeInsets.all(5),
+            width: Get.width,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                addWidth(8),
+                Container(
+                  height: 60,
+                  width: 3,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Color(0xff7ED957),
+                  ),
+                  // child: VerticalDivider(
+                  //   color: Color(0xffFF6100),
+                  //   thickness: 2.5,
+                  // ),
+                ),
+                const SizedBox(
+                  width: 13,
+                ),
+                SizedBox(
+                  height: 30,
+                  width: 30,
+                  child: CircleAvatar(
+                    backgroundColor: const Color(0xff7ED957),
+                    child: Text(
+                      remoteMessage.notification!.title == null ?
+                      'B' : remoteMessage.notification!.title.toString().substring(0,1),
+                      style: const TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 6),
+                        child: Text(
+                          remoteMessage.notification!.title ?? "",
+                          style: GoogleFonts.poppins(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0xff7ED957),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, top: 2),
+                        child: Text(
+                          remoteMessage.notification!.body ?? "",
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0xff000000),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        background: Colors.transparent,
+        elevation: 50,
+        contentPadding: EdgeInsets.zero
+    );
   }
 
   onMessageOpenApp(RemoteMessage event) {
